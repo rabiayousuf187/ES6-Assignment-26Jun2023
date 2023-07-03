ad3e1f79f9479a73a0a3a599305ee8692ded2856
rabiayousuf187 <rabiayousuf187@gmail.com>
2023-07-03 14:43:35 +0500

    done Chap 2 Ques 2

--- js/TEMPLATE-STRINGS.js
+++ js/TEMPLATE-STRINGS.js
@@ -16,5 +16,32 @@ function ques1(){
 }
 
 function ques2(){
+  function calculateSumAndAverage(numbers) {
+    let sum = 0;
+    for (let i = 0; i < numbers.length; i++) {
+      sum += numbers[i];
+    }
+    const average = sum / numbers.length;
+    return `Sum: ${sum}, Average: ${average}`;
+  }
+  
+ 
+  
+  
+  let maxlength = parseInt(prompt("Enter max length of Array,\nYou want to process:"));
+  let numbersArray =[];
+  
+  if (maxlength > 0) {
+    alert("Start Entering Arrays Elments:");
+    for(maxlength ; maxlength > 0 ; maxlength-- ){
+      numbersArray.push(parseInt(prompt("Enter of Array")) );
+    }
+    const result = calculateSumAndAverage(numbersArray);
+    console.log(result); // Output: Sum: 30, Average: 6
+    alert(result); // Output
+  }
+  else{
+    alert("Array Length must be Greater than 0!");
+  }
   
 }
