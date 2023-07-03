ff418d10bf98487bd4632f8cae41d9202fe93bb4
rabiayousuf187 <rabiayousuf187@gmail.com>
2023-07-03 13:15:18 +0500

    working on Chap 1 Ques 2

--- js/let-var-const.js
+++ js/let-var-const.js
@@ -7,3 +7,26 @@ function ques1(){
         alert(e);
     }
 }
+
+function ques2(){
+    function sumOfSquares(numbers) {
+        let sum = 0;
+      
+        for (let i = 0; i < numbers.length; i++) {
+          const square = numbers[i] ** 2;
+          sum += square;
+        }
+      
+        {
+          var additionalValue = 5;
+          sum += additionalValue;
+        }
+      
+        return sum;
+      }
+      
+      const numbersArray = [1, 2, 3, 4, 5];
+      const result = sumOfSquares(numbersArray);
+      console.log(result); // Output: 55
+      
+}
