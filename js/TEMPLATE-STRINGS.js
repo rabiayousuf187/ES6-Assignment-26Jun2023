702b99417508b394d77f17ba673bd69877cd8204
rabiayousuf187 <rabiayousuf187@gmail.com>
2023-07-03 14:38:24 +0500

    done Chap 2 Ques 1

--- js/TEMPLATE-STRINGS.js
+++ js/TEMPLATE-STRINGS.js
@@ -1,3 +1,16 @@
 function ques1(){
-    
+    function createGreeting(obj) {
+        var message = `Hello,${obj.name} ! You are ${obj.age} years old.`;
+        return message;
+      }
+      
+      
+      var person = {
+        name: prompt("Enter Your Name:"),
+        age: parseInt(prompt("Enter Your Age:")),
+      };
+      
+      var greeting = createGreeting(person);
+      alert(greeting); // Output: Hello, John! You are 25 years old.
+      
 }
