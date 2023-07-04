14c5e63dfdd987efacf3bf06356daf9350727434
rabiayousuf187 <rabiayousuf187@gmail.com>
2023-07-04 11:12:01 +0500

    working on Chap 3 Ques 3

--- js/DESTRUCTING.js
+++ js/DESTRUCTING.js
@@ -20,15 +20,59 @@ function ques2(){
         const { name, age, occupation } = person;
         const personInfo = `Person Name: ${name} is ${age} years old, occupation is ${occupation}`;
         return personInfo;
-      }
-      
-      const person = {
+    }
+    
+    const person = {
         name: prompt("Enter Your Name:"),
         age: parseInt(prompt("Enter Your Age:")),
         occupation: prompt("Enter Your Occupation:"),
-      };
+    };
+    
+    const personInfoString = createPersonInfo(person);
+    alert(personInfoString);
+    
+}
+
+function ques3(){
+    function findPersonWithHighestScore(people) {
+        const [firstPerson, ...restOfPeople] = people;
+        let highestScore = firstPerson.score;
+        let personWithHighestScore = firstPerson.name;
       
-      const personInfoString = createPersonInfo(person);
-      alert(personInfoString);
+        for (let i = 0; i < restOfPeople.length; i++) {
+          const { name, score } = restOfPeople[i];
+          if (score > highestScore) {
+            highestScore = score;
+            personWithHighestScore = name;
+          }
+        }
+      
+        return personWithHighestScore;
+      }
+      let maxlength = parseInt(prompt("Enter max length of Person Array,\nYou want to process:"));
+      let peopleArray = [], name, score;
+
+      if (maxlength > 0) {
+        alert("Start Entering Persons Details:");
+          for(maxlength ; maxlength > 0 ; maxlength-- ){
+              name = prompt("Enter of Name");
+              score = parseInt(prompt("Enter of Array"));
+
+            peopleArray.push({ name: name, score: score },);
+
+          }
+          console.log("peopleArray recorded ==== ",peopleArray);
+
+          let personWithHighestScore = findPersonWithHighestScore(peopleArray);
+          console.log(personWithHighestScore);
+      }
+      else{
+        alert("Array Length must be Greater than 0!");
+      }
+      
+      
+      
+     
+      // Output: Jane
       
 }
