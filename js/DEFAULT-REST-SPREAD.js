da2130e339c3df653a53b435b73110c4bf898588
rabiayousuf187 <rabiayousuf187@gmail.com>
2023-07-06 15:13:31 +0500

    done on Chap 4 Ques 1

--- js/DEFAULT-REST-SPREAD.js
+++ js/DEFAULT-REST-SPREAD.js
@@ -1,3 +1,14 @@
-function ques1(){
-    
+function ques1() {
+    function calculateSum(num1 = 0, num2 = 0) {
+        const sum = num1 + num2;
+        return sum;
+    }
+
+    alert("Sum of Two Numbers with Default:");
+    let num1 = parseInt(prompt("Enter of Number 1 Value:"));
+    let num2 = parseInt(prompt("Enter of Number 2 Value:"));
+
+    const result = calculateSum(num1 , num2);
+    alert(`Sum of ${num1} and ${num2} is   ${result}`);
+
 }
