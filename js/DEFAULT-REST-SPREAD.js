07906cad8ab13d30e481b3cc1b32d3fe760d987e
rabiayousuf187 <rabiayousuf187@gmail.com>
2023-07-12 15:07:30 +0500

    working on Chap 4 Ques 3

--- js/DEFAULT-REST-SPREAD.js
+++ js/DEFAULT-REST-SPREAD.js
@@ -25,5 +25,18 @@ function ques2(){
 }
 
 function ques3(){
+    let maxlength = parseInt(prompt("Enter max length of Array,\nYou want to process:"));
+      let numbersArray =[];
 
+      if (maxlength > 0) {
+        alert("Start Entering Arrays Elments:");
+          for(maxlength ; maxlength > 0 ; maxlength-- ){
+            numbersArray.push(parseInt(prompt("Enter of Array")) );
+          }
+          const maxValue = findMaxValue(numbersArray);
+          alert(`Maximum Value is ${maxValue}`); // Output
+      }
+      else{
+        alert("Array Length must be Greater than 0!");
+      }
 }
