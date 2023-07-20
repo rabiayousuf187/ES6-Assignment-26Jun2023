95a91b91a88b268a7876c0ebcba380817f03d165
rabiayousuf187 <rabiayousuf187@gmail.com>
2023-07-20 13:46:59 +0500

    done Chap 7 till Ques 1

--- js/MAP.js
+++ js/MAP.js
@@ -1,3 +1,25 @@
 function ques1(){
-    
+    function mapNumbersToSquares(numbers) {
+        const resultMap = new Map();
+        numbers.forEach((number) => {
+          resultMap.set(number, number * number);
+        });
+        return resultMap;
+      }
+
+      let maxlength = parseInt(prompt("Enter max length of Array,\nYou want to process:"));
+    let numbersArray = [];
+
+    if (maxlength > 0) {
+        alert("Start Entering Arrays Elments:");
+        for (maxlength; maxlength > 0; maxlength--) {
+            numbersArray.push(parseInt(prompt("Enter of Array")));
+        }
+    }
+    else {
+        alert("Array Length must be Greater than 0!");
+    }
+    const arraySquare = mapNumbersToSquares(numbersArray);
+    console.log(arraySquare); // Output
+      
 }
