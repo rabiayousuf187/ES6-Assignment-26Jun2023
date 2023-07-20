15908005b5abb7da01588070e540cb0a7cce2db1
rabiayousuf187 <rabiayousuf187@gmail.com>
2023-07-20 11:26:15 +0500

    done on Chap 5 Ques 4

--- js/ARROW-FUNCTIONS.js
+++ js/ARROW-FUNCTIONS.js
@@ -33,3 +33,30 @@ function ques3(){
 
     alert(`Product of ${num1} and ${num2} is ${multiply(num1 , num2)}`);
 }
+
+function ques4(){
+    const calculateAverage = (numbers) => {
+        if (numbers.length === 0) {
+          return 0; // To avoid division by zero for an empty array
+        }
+      
+        const sum = numbers.reduce((accumulator, currentNumber) => accumulator + currentNumber, 0);
+        return sum / numbers.length;
+      };
+      
+    let maxlength = parseInt(prompt("Enter max length of Array,\nYou want to process:"));
+    let numbersArray = [];
+
+    if (maxlength > 0) {
+        alert("Start Entering Arrays Elments:");
+        for (maxlength; maxlength > 0; maxlength--) {
+            numbersArray.push(parseInt(prompt("Enter of Array")));
+        }
+        
+    }
+    else {
+        alert("Array Length must be Greater than 0!");
+    }
+
+    alert("Average of Array "+ numbersArray + " is " + calculateAverage(numbersArray));
+}
