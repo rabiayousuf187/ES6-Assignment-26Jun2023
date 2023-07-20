63fa7d9815718b70b13cbbdb753bba9715be88ae
rabiayousuf187 <rabiayousuf187@gmail.com>
2023-07-20 11:07:00 +0500

    done on Chap 5 Ques 1

--- js/ARROW-FUNCTIONS.js
+++ js/ARROW-FUNCTIONS.js
@@ -1,3 +1,18 @@
 function ques1(){
+    function forEachExample(arr) {
+        // Check if the input is an array
+        if (!Array.isArray(arr)) {
+            console.error("Input must be an array");
+            return;
+        }
     
+        // Use the forEach method with an arrow function to log each element
+        arr.forEach((ele, ind)=> {
+            console.log(`Index = ${ind} has element: `,ele);
+        });
+    }
+    
+    // Example usage:
+    let arr = [1, 2, 3, 4, 5];
+    forEachExample(arr); 
 }
