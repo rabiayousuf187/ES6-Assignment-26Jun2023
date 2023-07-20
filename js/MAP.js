d94a3867a1ad143f5402bddb76d2e0e0aade379f
rabiayousuf187 <rabiayousuf187@gmail.com>
2023-07-20 14:19:21 +0500

    done Chap 7 Ques 3

--- js/MAP.js
+++ js/MAP.js
@@ -52,5 +52,36 @@ function ques2(){
 }
 
 function ques3(){
+    let maxlength = parseInt(prompt("Enter max length of Array,\nYou want to process:"));
+    let person = [];
     
+    function mapContainsKey(map, key) {
+        return map.has(key);
+      }
+      
+    function mapNamesToAges(people) {
+        const resultMap = new Map();
+        people.forEach((person) => {
+          resultMap.set(person.name, person.age);
+        });
+        return resultMap;
+      }
+    if (maxlength > 0) {
+        alert("Start Entering Person Details:");
+        for (let ind = 1; ind <= maxlength; ind++) {
+            person.push({
+                name: prompt(`Enter Person ${ind} Name:`),
+                age: parseInt(prompt(`Enter Person ${ind} Age:`))
+            });
+        }
+    }
+    else {
+        alert("Array Length must be Greater than 0!");
+    }
+
+    let key = prompt(`Enter Key to FInd in Map:`);
+    console.log(mapNamesToAges(person));
+    console.log(`key = ${key}`);
+    console.log(`key = ${mapContainsKey(mapNamesToAges(person), key)}`);
+
 }
