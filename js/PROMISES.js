fa49cd663377da98d8333e1d69c301d39da7f96a
rabiayousuf187 <rabiayousuf187@gmail.com>
2023-07-20 12:42:02 +0500

    done  Chap 6 Ques 2

--- js/PROMISES.js
+++ js/PROMISES.js
@@ -13,16 +13,34 @@ function ques1() {
     }
     alert("Find Square of any Number:");
     let number = parseInt(prompt("Enter of Number:"));
-  
+
     calculateSquareWithDelay(number)
         .then((result) => {
-           console.log('Square:', result);
+            console.log('Square:', result);
         })
         .catch((error) => {
             console.error('Error:', error.message);
         });
 }
 
-function ques2(){
-    
+function ques2() {
+    const promise1 = Promise.resolve(1);
+    const promise2 = Promise.resolve(2);
+    const promise3 = new Promise((resolve) => setTimeout(() => resolve(3), 1000));
+
+    const promisesArray = [promise1, promise2, promise3];
+
+    getAllResolvedValues(promisesArray)
+        .then((resolvedValues) => {
+            console.log(resolvedValues); // Output: [1, 2, 3] (after 1 second)
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+        });
+
+
+    function getAllResolvedValues(promisesArray) {
+        return Promise.all(promisesArray);
+    }
+
 }
