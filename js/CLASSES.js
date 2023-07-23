3c600ea66cd064538736dd2df948b0e2410441a1
rabiayousuf187 <rabiayousuf187@gmail.com>
2023-07-23 20:13:57 +0500

    Recovered changes after index corruption

--- js/CLASSES.js
+++ js/CLASSES.js
@@ -1,3 +1,17 @@
-function ques1(){
-    
+function ques1() {
+  // Step 1: Create the Person class
+  class Person {
+    constructor(firstname, lastname) {
+      this.firstname = firstname;
+      this.lastname = lastname;
+    }
+  }
+ 
+  // Step 2: Create instances of friends using the Person class
+  const friend1 = new Person("John", "Doe");
+  const friend2 = new Person("Jane", "Smith");
+  
+  // Step 3: Output both friends' names with a greeting
+  console.log(`Hello, ${friend1.firstname} ${friend1.lastname}!`);
+  console.log(`Hello, ${friend2.firstname} ${friend2.lastname}!`);
 }
