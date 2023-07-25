e8b87e02a63fb26242e8b8e66afab30c0fba68a5
rabiayousuf187 <rabiayousuf187@gmail.com>
2023-07-25 15:48:15 +0500

    done on Chap 11 Ques 1

--- js/FOR-OF-LOOP.js
+++ js/FOR-OF-LOOP.js
@@ -1,3 +1,9 @@
-function ques1(){
-    
+function ques1() {
+    // Create an array called `fruits` with some fruit names
+    const fruits = ['apple', 'banana', 'orange', 'kiwi'];
+
+    // Use a `for...of` loop to iterate over the array and log each fruit name
+    for (const fruit of fruits) {
+        console.log(fruit);
+    }
 }
