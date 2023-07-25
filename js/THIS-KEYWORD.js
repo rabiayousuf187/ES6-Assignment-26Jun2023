8d91bf49f2b2116c4f6e65d42c69b4566b67670a
rabiayousuf187 <rabiayousuf187@gmail.com>
2023-07-25 15:22:12 +0500

    done on Chap 10 Ques 1

--- js/THIS-KEYWORD.js
+++ js/THIS-KEYWORD.js
@@ -1,3 +1,13 @@
 function ques1(){
-    
+    // Create an object called person
+const person = {
+    name: "John",
+    greet() {
+      // Log a greeting message with the person's name
+      console.log(`Hello, ${this.name}!`);
+    }
+  };
+  
+  // Call the greet method
+  person.greet();
 }
