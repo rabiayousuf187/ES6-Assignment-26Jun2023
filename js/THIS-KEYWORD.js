319ee19139ebefa2fd8abcc82d7debe538b378cd
rabiayousuf187 <rabiayousuf187@gmail.com>
2023-07-25 15:24:42 +0500

    done on Chap 10 Ques 2

--- js/THIS-KEYWORD.js
+++ js/THIS-KEYWORD.js
@@ -13,5 +13,24 @@ const person = {
 }
 
 function ques2(){
-    
+    class Car {
+        constructor(brand, speed) {
+          this.brand = brand;
+          this.speed = speed;
+        }
+      
+        accelerate(value) {
+          this.speed += value;
+        }
+      
+        printSpeed() {
+          console.log(`Current speed of the ${this.brand} car is ${this.speed} km/h.`);
+        }
+      }
+      
+      // Example usage
+      const myCar = new Car("Toyota", 60);
+      myCar.printSpeed(); // Output: Current speed of the Toyota car is 60 km/h.
+      myCar.accelerate(40);
+      myCar.printSpeed(); // Output: Current speed of the Toyota car is 100 km/h.
 }
