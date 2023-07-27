6fb92cd04383d3e057abef1a7c8ac80d9bc3573d
rabiayousuf187 <rabiayousuf187@gmail.com>
2023-07-27 10:07:14 +0500

    done Chap 12 Ques 1 part A

---
+++ js/FOR-IN-LOOP.js
@@ -0,0 +1,8 @@
+function ques1() {
+    // Step 1: Create a simple object with three items
+    const myObject = {
+        name: "John Doe",
+        age: 30,
+        occupation: "Engineer",
+    };
+}
