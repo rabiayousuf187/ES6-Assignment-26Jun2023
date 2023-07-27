a48fc2440936f4320317cc5d137afc8d3ce320cb
rabiayousuf187 <rabiayousuf187@gmail.com>
2023-07-27 15:05:07 +0500

    done on Chap 14 Ques 2

--- js/CLOSURES.js
+++ js/CLOSURES.js
@@ -21,5 +21,21 @@ function ques1() {
 }
 
 function ques2(){
-    
+    function createMultiplier(factor) {
+        // The factor parameter is within the scope of the outer function
+      
+        function multiply(number) {
+          return number * factor; // Access the factor parameter using closures
+        }
+      
+        return multiply;
+      }
+      
+      // Example of using the createMultiplier function
+      const multiplyByTwo = createMultiplier(2);
+      console.log(multiplyByTwo(5)); // Output: 10 (5 * 2)
+      
+      const multiplyByThree = createMultiplier(3);
+      console.log(multiplyByThree(5)); // Output: 15 (5 * 3)
+      
 }
