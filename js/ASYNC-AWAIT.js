1a806fd67a5228741f7ef0ca28849326b64acfcb
rabiayousuf187 <rabiayousuf187@gmail.com>
2023-07-27 11:39:54 +0500

    done on Chap 13 Ques 1

--- js/ASYNC-AWAIT.js
+++ js/ASYNC-AWAIT.js
@@ -1,3 +1,25 @@
-function ques1(){
-    
+function ques1() {
+    function delayedGreeting(name) {
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                if (typeof name !== 'string') {
+                    reject(new Error('Name must be a string'));
+                } else {
+                    resolve(`Hello, ${name}!`);
+                }
+            }, 2000);
+        });
+    }
+
+    async function greet() {
+        try {
+            let name = prompt("Enter Name: ");
+            const greeting = await delayedGreeting(name);
+            console.log(greeting);
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    greet();
 }
