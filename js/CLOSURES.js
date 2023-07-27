c7f783e9ebed98b920472084c68f7c760b6401c2
rabiayousuf187 <rabiayousuf187@gmail.com>
2023-07-27 14:59:34 +0500

    done on Chap 14 Ques 1

--- js/CLOSURES.js
+++ js/CLOSURES.js
@@ -1,3 +1,21 @@
-function ques1(){
-    
+function ques1() {
+    function counter() {
+        let count = 0; // The counter variable is within the scope of the outer function
+
+        function incrementAndLog() {
+            count++; // Increment the counter
+            console.log("Counter value:", count);
+        }
+
+        return incrementAndLog;
+    }
+
+    // Example of using the counter function to create an incrementing counter
+    const incrementCounter = counter();
+
+    // Call the returned function multiple times to see the counter in action
+    incrementCounter(); // Output: Counter value: 1
+    incrementCounter(); // Output: Counter value: 2
+    incrementCounter(); // Output: Counter value: 3
+
 }
