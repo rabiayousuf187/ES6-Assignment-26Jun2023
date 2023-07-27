00eba0b6e658204c5bfb7d2cd5acf035e16e5f5b
rabiayousuf187 <rabiayousuf187@gmail.com>
2023-07-27 15:06:49 +0500

    working on Chap 14 Ques 3

--- js/CLOSURES.js
+++ js/CLOSURES.js
@@ -20,22 +20,39 @@ function ques1() {
 
 }
 
-function ques2(){
+function ques2() {
     function createMultiplier(factor) {
         // The factor parameter is within the scope of the outer function
-      
+
         function multiply(number) {
-          return number * factor; // Access the factor parameter using closures
+            return number * factor; // Access the factor parameter using closures
         }
-      
+
         return multiply;
-      }
-      
-      // Example of using the createMultiplier function
-      const multiplyByTwo = createMultiplier(2);
-      console.log(multiplyByTwo(5)); // Output: 10 (5 * 2)
-      
-      const multiplyByThree = createMultiplier(3);
-      console.log(multiplyByThree(5)); // Output: 15 (5 * 3)
-      
+    }
+
+    // Example of using the createMultiplier function
+    const multiplyByTwo = createMultiplier(2);
+    console.log(multiplyByTwo(5)); // Output: 10 (5 * 2)
+
+    const multiplyByThree = createMultiplier(3);
+    console.log(multiplyByThree(5)); // Output: 15 (5 * 3)
+
+}
+
+function ques3() {
+    function secretMessage(message) {
+        // The message parameter is within the scope of the outer function
+
+        function logSecret() {
+            console.log("Secret Message:", message); // Access the message parameter using closures
+        }
+
+        return logSecret;
+    }
+
+    // Example of using the secretMessage function
+    const secret = secretMessage("Hello, this is a secret!");
+    secret(); // Output: Secret Message: Hello, this is a secret!
+
 }
